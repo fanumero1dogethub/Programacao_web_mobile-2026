11042b225dd61e2b9e8442463d7c774ea47ae45c
fanumero1dogethub <fanumerodoappinventor@gmail.com>
2026-03-29 18:20:28 +0000

    adicionando projeto Dart

---
+++ fundamentos_de_dart/bin/olamundo.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+//Programa Principal
+void main(){
+  // Exibe uma mensagem na tela e pula uma linha
+    print("Faculdade de Tecnologia de Ourinhos");
+    print("Olá mundo");
+
+    stdout.write("Fundamentos de dart");
+}
